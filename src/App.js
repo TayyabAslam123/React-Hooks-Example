b04eb5766192ada124996500961cfd01f5597942
TayyabAslam123 <tayyabaslam771@gmail.com>
2023-07-19 01:13:20 -0700

    useImperativeHandle

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import './App.css';
 // import UseRefHook from './Components/UseRefHook';
 // import UseRefHook2 from './Components/UseRefHook2';
 // import UseRefHook5 from './Components/UseRefHook5';
-import UseLayoutEffectHook from './Components/UseLayoutEffectHook';
-
+// import UseLayoutEffectHook from './Components/UseLayoutEffectHook';
+import UseImperativeHandleHook from './Components/UseImperativeHandleHook';
 
 function App() {
   return (
-    <UseLayoutEffectHook/>
+    <UseImperativeHandleHook/>
   );
 }
 
